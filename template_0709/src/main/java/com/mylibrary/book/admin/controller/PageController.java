3c3c962189efa1a31b7a49a7eb24c0c9a01f5854
qlqldldh <60098657+qlqldldh@users.noreply.github.com>
2020-07-14 23:10:19 +0900

    Update PageController.java

--- template_0709/src/main/java/com/mylibrary/book/admin/controller/PageController.java
+++ template_0709/src/main/java/com/mylibrary/book/admin/controller/PageController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 // summary methods for panning pages
 @Controller
 @RequestMapping("/admin")
