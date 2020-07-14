2479cdb6f89e35975375aa60f61a7804f9edd809
qlqldldh <60098657+qlqldldh@users.noreply.github.com>
2020-07-14 23:16:59 +0900

    Update PageController.java

--- template_0709/src/main/java/com/mylibrary/book/admin/controller/PageController.java
+++ template_0709/src/main/java/com/mylibrary/book/admin/controller/PageController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class PageController {
-	
 	/* Page-Panning Controller */
 	
 	@RequestMapping("/index")
